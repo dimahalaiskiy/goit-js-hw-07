eeb472cde0adb880939816ef82e7d8a637469059
Dima Halaiskiy <dgalaiskiy@icloud.com>
2021-10-21 10:51:20 +0300

    remove addeventlistener

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,7 +5,6 @@ console.log(galleryItems);
 
 const divGalleryEl = document.querySelector('.gallery')
 
-
 function galleryMarkup() {
        return galleryItems.map(( {preview, original, description} ) => {
         return `<a class="gallery__item" href=${original}>
@@ -16,17 +15,5 @@ function galleryMarkup() {
 
 divGalleryEl.insertAdjacentHTML('beforeend', galleryMarkup(galleryItems))
 
-const onClickCurrentPhoto = document.querySelector('.gallery__image')
-
-
-function openCurrentOriginalPhoto(e) {
-    e.preventDefault()
-    let gallery = new SimpleLightbox('.gallery a');
-    gallery.on('show.simplelightbox', function () {})
-    gallery.on('error.simplelightbox', function (e) {
-	console.log(e); 
-    });
-}
-
-
-onClickCurrentPhoto.addEventListener('click', openCurrentOriginalPhoto)
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {})
