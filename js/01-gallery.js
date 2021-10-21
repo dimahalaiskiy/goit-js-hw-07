d1f0d6ba0bbe82166bb95a1c37d5f0b31a6ce176
Dima Halaiskiy <dgalaiskiy@icloud.com>
2021-10-21 10:54:01 +0300

    add lazy load for images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,7 +10,7 @@ function galleryMarkup() {
        return galleryItems.map(( {preview, original, description} ) => {
         return `<div class="gallery__item">
         <a class="gallery__link" href=${original}>
-            <img class="gallery__image" src=${preview}
+            <img class="gallery__image" loading="lazy" src=${preview}
             data-source=${original}alt=${description}/></a>
         </div>`
     }).join('')
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ const divGalleryEl = document.querySelector('.gallery')
 function galleryMarkup() {
        return galleryItems.map(( {preview, original, description} ) => {
         return `<a class="gallery__item" href=${original}>
-        <img class="gallery__image" src=${preview} alt=${description} title=${description}/>
+        <img class="gallery__image" loading="lazy" src=${preview} alt=${description} title=${description}/>
       </a>`
     }).join('')
 }
